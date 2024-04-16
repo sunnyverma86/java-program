e83fbd863bc89d3aedb91c6a0c2f759477f77732
sunnyverma86 <61960321+sunnyverma86@users.noreply.github.com>
2024-04-16 18:41:54 +0530

    Initial commit

---
+++ MyClass.java
@@ -0,0 +1,11 @@
+import java.time.Instant;
+public class MyClass
+{
+	public static void main(String...r)
+	{
+		System.out.println("hello World");
+		System.out.println(Instant.now());
+		System.out.println("no where home jenkins");
+	}
+}
+
